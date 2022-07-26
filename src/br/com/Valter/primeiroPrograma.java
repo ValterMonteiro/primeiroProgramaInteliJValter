1fb670f3f4a3ef540ee078d21cd69f121b441bc1
ValterMonteiro <valtinms@gmail.com>
2022-07-26 16:21:02 -0400

    project creator

---
+++ src/br/com/Valter/primeiroPrograma.java
@@ -0,0 +1,15 @@
+package br.com.Valter;
+
+import br.com.Valter.model.Gato;
+
+public class primeiroPrograma {
+    public static void main(String[] args) {
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World! " + (a + b));*/
+    }
+}
