b1c61a255937643de94e1aa93c0381037060f5fe
ValterMonteiro <valtinms@gmail.com>
2022-07-26 16:42:03 -0400

    new class livro

--- src/br/com/Valter/primeiroPrograma.java
+++ src/br/com/Valter/primeiroPrograma.java
@@ -2,14 +2,66 @@ package br.com.Valter;
 
 import br.com.Valter.model.Gato;
 
+import java.util.Objects;
+
 public class primeiroPrograma {
     public static void main(String[] args) {
         Gato gato = new Gato();
 
         System.out.println(gato);
 
+        Livro livro1 = new Livro("Qualquer livro",  300);
+        System.out.println(livro1);
+
         /*int a = 5;
         int b = 3;
         System.out.println("Hello World! " + (a + b));*/
+    }
+}
+
+class Livro {
+    private String nome;
+    private Integer numPag;
+
+    public Livro(String nome, Integer numPag) {
+        this.nome = nome;
+        this.numPag = numPag;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Integer getNumPag() {
+        return numPag;
+    }
+
+    public void setNumPag(Integer numPag) {
+        this.numPag = numPag;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Livro livro = (Livro) o;
+        return Objects.equals(nome, livro.nome) && Objects.equals(numPag, livro.numPag);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nome, numPag);
+    }
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "nome='" + nome + '\'' +
+                ", numPag=" + numPag +
+                '}';
     }
 }
